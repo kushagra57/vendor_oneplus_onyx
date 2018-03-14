7ed9d3fdc33b1f458fb42f88521bfeb3fbf46c02
YumeMichi <do4suki@gmail.com>
2018-03-14 16:32:03 +0800

    Revert "onyx: Ship OnePlus Camera"
    This reverts commit 9faf585e579a95af4ab199c73e339b8b365db5c2.
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- onyx/onyx-vendor.mk
+++ onyx/onyx-vendor.mk
@@ -18,7 +18,6 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/onyx/proprietary/etc/firmware/tfa98xx.cnt:system/etc/firmware/tfa98xx.cnt \
     vendor/oneplus/onyx/proprietary/etc/firmware/tp/15055/15055_FW_S3508_Tpk.img:system/etc/firmware/tp/15055/15055_FW_S3508_Tpk.img \
     vendor/oneplus/onyx/proprietary/etc/firmware/tp/15055/15055_Limit_Tpk.img:system/etc/firmware/tp/15055/15055_Limit_Tpk.img \
-    vendor/oneplus/onyx/proprietary/priv-app/OnePlusCamera/lib/arm/libopbaselib.so:system/priv-app/OnePlusCamera/lib/arm/libopbaselib.so \
     vendor/oneplus/onyx/proprietary/vendor/bin/adsprpcd:system/vendor/bin/adsprpcd \
     vendor/oneplus/onyx/proprietary/vendor/bin/charger_monitor:system/vendor/bin/charger_monitor \
     vendor/oneplus/onyx/proprietary/vendor/bin/hci_qcomm_init:system/vendor/bin/hci_qcomm_init \
@@ -207,7 +206,6 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/onyx/proprietary/vendor/lib/libFNVfbEngineLib.so:system/vendor/lib/libFNVfbEngineLib.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libfastcvadsp_stub.so:system/vendor/lib/libfastcvadsp_stub.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libfastcvopt.so:system/vendor/lib/libfastcvopt.so \
-    vendor/oneplus/onyx/proprietary/vendor/lib/libfilter-sdk.so:system/vendor/lib/libfilter-sdk.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libflp.so:system/vendor/lib/libflp.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libgeofence.so:system/vendor/lib/libgeofence.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
@@ -224,7 +222,6 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/onyx/proprietary/vendor/lib/liblqe.so:system/vendor/lib/liblqe.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libmdmdetect.so:system/vendor/lib/libmdmdetect.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libmm-abl-oem.so:system/vendor/lib/libmm-abl-oem.so \
-    vendor/oneplus/onyx/proprietary/vendor/lib/libmm-qcamera.so:system/vendor/lib/libmm-qcamera.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libmmcamera_interface.so:system/vendor/lib/libmmcamera_interface.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libmmcamera2_c2d_module.so:system/vendor/lib/libmmcamera2_c2d_module.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libmmcamera2_cpp_module.so:system/vendor/lib/libmmcamera2_cpp_module.so \
@@ -270,10 +267,7 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/onyx/proprietary/vendor/lib/libmmqjpeg_codec.so:system/vendor/lib/libmmqjpeg_codec.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libnetmgr.so:system/vendor/lib/libnetmgr.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/liboemcamera.so:system/vendor/lib/liboemcamera.so \
-    vendor/oneplus/onyx/proprietary/vendor/lib/libopbaselib.so:system/vendor/lib/libopbaselib.so \
-    vendor/oneplus/onyx/proprietary/vendor/lib/libopcamera.so:system/vendor/lib/libopcamera.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libopcamerahw_interface.so:system/vendor/lib/libopcamerahw_interface.so \
-    vendor/oneplus/onyx/proprietary/vendor/lib/libopcameralib.so:system/vendor/lib/libopcameralib.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libparam.so:system/vendor/lib/libparam.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libperipheral_client.so:system/vendor/lib/libperipheral_client.so \
     vendor/oneplus/onyx/proprietary/vendor/lib/libqcci_legacy.so:system/vendor/lib/libqcci_legacy.so \
@@ -325,12 +319,9 @@ PRODUCT_PACKAGES += \
     libTimeService \
     libmm-abl \
     libtime_genoff \
-    OnePlusCamera \
-    OnePlusCameraService \
     TimeService \
     shutdownlistener \
     qcrilmsgtunnel \
     ConnectivityExt \
-    org.codeaurora.camera \
     qcnvitems \
     qcrilhook
